3425322c0ea698269d7d70e7e64debc700ffb457
Ubuntu <ubuntu@ip-172-31-1-201.us-west-2.compute.internal>
2017-10-28 12:16:51 +0000

    Rebuilding OTP from git source code

---
+++ restart-otp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Restart OTP Grizzly server on port 8888
+MAILTO=""
+sudo killall java
+sleep 10
+java -Xmx6G -Xverify:none -jar /srv/vta.amigocloud.com/OTP/target/otp-1.0.0-shaded.jar --basePath /srv/vta.amigocloud.com/data/ --router default --server --port 8888 --cache /srv/vta.amigocloud.com/ned --autoReload --autoScan | tee /srv/vta.amigocloud.com/logs/grizzly-`date +%F`.log &
+res=$?
+if test "$res" = "0" ; then
+    echo "Restart successful and logging to `pwd`/logs/grizzy-`date +%F`.log"
+else
+    echo "Restart failed with exit code: $res"
+fi
+exit
