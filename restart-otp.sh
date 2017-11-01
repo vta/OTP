816f0b8efd3d69d6d9a2a862ed2b2554c3a09897
linuxwebexpert <Linux.Web.Expert@Gmail.com>
2017-11-01 10:53:56 -0700

    Improved restart script - kill grizzly server only

--- restart-otp.sh
+++ restart-otp.sh
@@ -2,9 +2,9 @@
 #
 # Restart OTP Grizzly server on port 8888
 MAILTO=""
-sudo killall java
-sleep 7
-java -Xmx6G -Xverify:none -jar /srv/vta.amigocloud.com/OTP/target/otp-1.0.0-shaded.jar --basePath /srv/vta.amigocloud.com/data/ --router default --server --port 8888 --cache /srv/vta.amigocloud.com/ned --autoReload --autoScan | tee /home/ubuntu/logs/grizzly-`date +%F`.log &
+sudo kill -9 `sudo ps ax | grep java | grep vta | grep 8888 | awk '{print $1}' | tr '\n' ' '`
+sleep 10
+java -Xmx6G -Xverify:none -jar /srv/vta.amigocloud.com/OTP/target/otp-1.0.0-shaded.jar --basePath /srv/vta.amigocloud.com/data/ --router default --server --port 8888 --cache /srv/vta.amigocloud.com/ned --autoReload --autoScan | tee /srv/vta.amigocloud.com/logs/grizzly-`date +%F`.log &
 res=$?
 if test "$res" = "0" ; then
     echo "Restart successful and logging to `pwd`/logs/grizzy-`date +%F`.log"
